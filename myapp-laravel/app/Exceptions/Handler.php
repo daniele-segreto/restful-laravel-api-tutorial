df2e4c422bb1ff684021ada1b58f51759110f627
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-02-23 11:07:59 +0100

    correzione errore su Handler.php + php artisan make:migration --table=users adds_api_token_to_users_table

--- myapp-laravel/app/Exceptions/Handler.php
+++ myapp-laravel/app/Exceptions/Handler.php
@@ -3,14 +3,15 @@
 namespace App\Exceptions;
 
 use Exception;
-use Throwable;
+use Throwable; // Importa la classe Throwable
+
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * Elenco degli input che non vengono mai salvati nella sessione durante le eccezioni di validazione.
      *
      * @var array<int, string>
      */
@@ -21,7 +22,7 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * Registra i callback di gestione delle eccezioni per l'applicazione.
      */
     public function register(): void
     {
@@ -30,18 +31,14 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function render($request, Exception $exception)
+    /**
+     * Gestisce la resa della risposta per le eccezioni.
+     */
+    public function render($request, Throwable $exception)
     {
-        // Questo sostituirà la nostra risposta 404 con
-        // una risposta JSON.
-        if (
-            $exception instanceof ModelNotFoundException &&
-            $request->wantsJson()
-        ) {
-            // Se l'eccezione è del tipo ModelNotFoundException
-            // e la richiesta vuole una risposta JSON,
-            // restituiamo una risposta JSON con un messaggio
-            // "Risorsa non trovata" e lo status code 404.
+        // Se l'eccezione è del tipo ModelNotFoundException e la richiesta vuole una risposta JSON,
+        // restituiamo una risposta JSON con un messaggio "Risorsa non trovata" e lo status code 404.
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
             return response()->json([
                 'data' => 'Risorsa non trovata'
             ], 404);
@@ -50,19 +47,4 @@ class Handler extends ExceptionHandler
         // Altrimenti, restituisci la gestione predefinita delle eccezioni.
         return parent::render($request, $exception);
     }
-
-
-    // public function render($request, Exception $exception)
-    // {
-    //     // This will replace our 404 response with
-    //     // a JSON response.
-    //     if ($exception instanceof ModelNotFoundException) {
-    //         return response()->json([
-    //             'error' => 'Resource not found'
-    //         ], 404);
-    //     }
-
-    //     return parent::render($request, $exception);
-    // }
-
 }
