8df23b60eb0857dcb0bfc7b4acbfdb9670a4abbb
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-02-23 11:35:51 +0100

    aggiunta la colonna api token alla tabella users

--- myapp-laravel/database/migrations/2024_02_23_100122_adds_api_token_to_users_table.php
+++ myapp-laravel/database/migrations/2024_02_23_100122_adds_api_token_to_users_table.php
@@ -1,28 +1,28 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration; // Importa la classe Migration dal namespace Illuminate\Database\Migrations
+use Illuminate\Database\Schema\Blueprint; // Importa la classe Blueprint dal namespace Illuminate\Database\Schema
+use Illuminate\Support\Facades\Schema; // Importa la classe Schema dal namespace Illuminate\Support\Facades
 
-return new class extends Migration
+return new class extends Migration // Restituisce una nuova istanza di una classe anonima che estende Migration
 {
     /**
-     * Run the migrations.
+     * Esegue le migrazioni.
      */
-    public function up(): void
+    public function up() // Definisce il metodo up, che viene chiamato quando si esegue la migrazione
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) { // Utilizza il metodo table del Schema per definire la struttura della tabella 'users'
+            $table->string('api_token', 60)->unique()->nullable(); // Aggiunge una colonna di tipo stringa chiamata 'api_token' con una lunghezza massima di 60 caratteri, univoca e consentendo valori nulli
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Annulla le migrazioni.
      */
-    public function down(): void
+    public function down() // Definisce il metodo down, che viene chiamato quando si esegue il rollback della migrazione
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) { // Utilizza il metodo table del Schema per definire la struttura della tabella 'users'
+            $table->dropColumn(['api_token']); // Rimuove la colonna 'api_token' dalla tabella 'users'
         });
     }
 };
