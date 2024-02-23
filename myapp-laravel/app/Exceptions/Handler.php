e88932bd6cc912c9b3d2de336e18fc2afaaa93e7
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-02-23 10:43:08 +0100

    invio di una risposta 404 corretta

--- myapp-laravel/app/Exceptions/Handler.php
+++ myapp-laravel/app/Exceptions/Handler.php
@@ -2,8 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -27,4 +29,40 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Exception $exception)
+    {
+        // Questo sostituirà la nostra risposta 404 con
+        // una risposta JSON.
+        if (
+            $exception instanceof ModelNotFoundException &&
+            $request->wantsJson()
+        ) {
+            // Se l'eccezione è del tipo ModelNotFoundException
+            // e la richiesta vuole una risposta JSON,
+            // restituiamo una risposta JSON con un messaggio
+            // "Risorsa non trovata" e lo status code 404.
+            return response()->json([
+                'data' => 'Risorsa non trovata'
+            ], 404);
+        }
+
+        // Altrimenti, restituisci la gestione predefinita delle eccezioni.
+        return parent::render($request, $exception);
+    }
+
+
+    // public function render($request, Exception $exception)
+    // {
+    //     // This will replace our 404 response with
+    //     // a JSON response.
+    //     if ($exception instanceof ModelNotFoundException) {
+    //         return response()->json([
+    //             'error' => 'Resource not found'
+    //         ], 404);
+    //     }
+
+    //     return parent::render($request, $exception);
+    // }
+
 }
